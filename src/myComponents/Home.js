cafd6416adfa922b573900071849afec5d0be47d
shiv4ngi <come2shivangu@gmail.com>
2021-09-12 20:10:33 +0530

    Initial commit

---
+++ src/myComponents/Home.js
@@ -0,0 +1,13 @@
+import Album from "./Album";
+import ControlledCarousel from "./ControlledCarousel";
+
+function Home() {
+  return (
+    <div>
+      <ControlledCarousel />
+      <Album />
+    </div>
+  );
+}
+
+export default Home;
